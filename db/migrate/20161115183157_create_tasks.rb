76d92a6edaaadd12cf66377776145380982f72d0
Micaela <mika_peralta93@yahoo.com>
2016-11-29 02:19:40 -0300

    correccion migracion cambio de nombre de la tabla

--- db/migrate/20161115183157_create_tasks.rb
+++ db/migrate/20161115183157_create_tasks.rb
@@ -7,7 +7,7 @@ class CreateTasks < ActiveRecord::Migration[5.0]
      
       t.string :type
       
-      t.belongs_to :to_do_list, foreign_key: true,  index: true 
+      t.belongs_to :list, foreign_key: true,  index: true 
       t.integer :progress 
       t.date :date_begin, :date_end
 
